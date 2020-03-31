1fe98f89e0c88e0e26a0c3f0c92cd8e41ba3037c
Greta Castillo <greta.castillo@icloud.com>
2020-03-31 15:15:29 -0400

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular9-web304-grocery-list-app';
+  title = 'Web 304: Angular';
 }
